ba641d40182d35adb2ab30b42f75dfe07d24e371
Philip Yates <PKYates@student.fullsail.edu>
2021-05-16 21:06:22 -0500

    Two test remain!!

--- DSA_Labs/Dictionary.h
+++ DSA_Labs/Dictionary.h
@@ -37,17 +37,17 @@ NOTE: If the unit test is not on, that code will not be compiled!
 #define LAB_5	1
 
 // Individual unit test toggles
-#define DICT_CTOR					1
-#define DICT_PAIR_CTOR				0
-#define DICT_DTOR					0
-#define DICT_CLEAR					0
-#define DICT_INSERT					0
-#define DICT_INSERT_EXISTING		0
-#define DICT_FIND					0
-#define DICT_FIND_NOT_FOUND			0
-#define DICT_REMOVE					0
-#define DICT_REMOVE_NOT_FOUND		0
-#define DICT_ASSIGNMENT_OP			0
+#define DICT_CTOR					0 //Passing
+#define DICT_PAIR_CTOR				0 //Passing
+#define DICT_DTOR					0 //Passing
+#define DICT_CLEAR					0 
+#define DICT_INSERT					0 //Passing
+#define DICT_INSERT_EXISTING		0 //Passing
+#define DICT_FIND					0 //Passing
+#define DICT_FIND_NOT_FOUND			0 //Passing
+#define DICT_REMOVE					0 //Passing
+#define DICT_REMOVE_NOT_FOUND		0 //Passing
+#define DICT_ASSIGNMENT_OP			1 //Passing
 #define DICT_COPY_CTOR				0
 
 /************/
@@ -56,7 +56,7 @@ NOTE: If the unit test is not on, that code will not be compiled!
 #include <list>
 
 #if LAB_5
-
+//Key First Value 2nd
 template<typename Key, typename Value>
 class Dictionary {
 
@@ -72,6 +72,8 @@ class Dictionary {
 		//		_value
 		Pair(const Key& _key, const Value& _value) {
 			// TODO: Implement this method
+			key = _key;
+			value = _value;
 		}
 
 		// For testing
@@ -91,14 +93,20 @@ public:
 	// Constructor
 	// In:	_numBuckets			The number of elements to allocate
 	//		_hashFunc			The hashing function to be used
-	Dictionary(size_t _numBuckets, unsigned int (*_hashFunc)(const Key&))  {
+	Dictionary(size_t _numBuckets, unsigned int (*_hashFunc)(const Key&)) {
 		// TODO: Implement this method
+
+		mNumBuckets = _numBuckets;
+		mHashFunc = _hashFunc;
+		mTable = new std::list<Pair>[_numBuckets];
+
 	}
 
 	// Destructor
 	//		Cleans up any dynamically allocated memory
 	~Dictionary() {
 		// TODO: Implement this method
+		delete[] mTable;
 	}
 
 	// Copy constructor
@@ -106,7 +114,8 @@ public:
 	// In:	_copy				The object to copy from
 	Dictionary(const Dictionary& _copy) {
 		// TODO: Implement this method
-
+		
+	
 	}
 
 	// Assignment operator
@@ -117,14 +126,44 @@ public:
 	//		This allows us to daisy-chain
 	Dictionary& operator=(const Dictionary& _assign) {
 		// TODO: Implement this method
-	}
+		delete[] mTable;
+		mTable = new std::list<Pair>[_assign.mNumBuckets];
+		if (this != &_assign)
+		{
+			this->mNumBuckets = _assign.mNumBuckets;
+			this->mHashFunc = _assign.mHashFunc;
+			for (int i = 0; i < mNumBuckets; i++)
+			{
+				
+					mTable[i] = _assign.mTable[i];	
+			}
+		}
+
+		return *this;
+		}
+
+
+
 
 	// Clear
 	//		Clears all internal data being stored
 	//  NOTE:	Does not delete table or reset hash function
 	void Clear() {
 		// TODO: Implement this method
+
+		for (int i = 0; i < mNumBuckets; i++)
+		{
+			for (typename std::list<Pair>::iterator it = mTable[i].begin(); it != mTable[i].end(); ++it)
+			{
+				if (it->key != NULL) {
+					it->key = NULL;
+					it->value = NULL;
+					
+				}
+			}
+		}
 	}
+
 
 	// Insert an item into the table
 	// In:	_key		The key to add at	
@@ -133,6 +172,25 @@ public:
 	// NOTE:	If there is already an item at the provided key, overwrite it.
 	void Insert(const Key& _key, const Value& _value) {
 		// TODO: Implement this method
+		//Call Hash Function
+		int hashValue = mHashFunc(_key);
+		auto& tableCell = mTable[hashValue];
+		auto Itr = begin(tableCell);
+		bool keyExist = false;
+		for (; Itr != end(tableCell); ++Itr) {
+			if (Itr->key == _key) {
+				keyExist = true;
+				Itr->value = _value;
+				break;
+			}
+		}
+		if (!keyExist) {
+			tableCell.emplace_back(_key, _value);
+		}
+		return;
+		
+
+		
 	}
 
 	// Find a value at a specified key
@@ -142,6 +200,43 @@ public:
 	// NOTE:		Return a null pointer if key is not present
 	const Value* Find(const Key& _key) {
 		// TODO: Implement this method
+		//Call Has function
+
+		int hashValue = mHashFunc(_key);
+		auto& tableCell = mTable[hashValue];
+		auto Itr = begin(tableCell);
+		bool keyExist = false;
+		for (; Itr != end(tableCell); ++Itr) {
+			if (Itr->key == _key) {
+				keyExist = true;
+				
+				return &Itr->value;
+				break;
+			}
+		}
+		if (!keyExist) {
+			return nullptr;
+		}
+	
+	
+
+
+		//for (; Itr != end(tableCell); Itr++) {
+
+		//	//Key exist
+		//	if (Itr->key == _key && _key != NULL) {
+
+		//		const Value& ptr = Itr->value ;
+		//		return &ptr;
+		//	}
+		//	
+		//	//Key dose not exist ptr needs to return a null //**DONE**
+		//	else {
+		//		return nullptr;
+		//	}
+		//
+		//}
+
 	}
 
 	// Remove a value at a specified key
@@ -150,6 +245,15 @@ public:
 	// Return: True, if an item was removed
 	bool Remove(const Key& _key) {
 		// TODO: Implement this method
+		int currBucket = mHashFunc(_key);
+		for (typename std::list<Pair>::iterator it = mTable[currBucket].begin(); it != mTable[currBucket].end(); ++it)
+		{
+			if (it->key == _key) {
+				mTable[currBucket].erase(it);
+				return true;
+			}
+		}
+		return false;
 	}
 };
 
